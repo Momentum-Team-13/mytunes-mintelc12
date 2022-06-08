5df64dad71ce74d6b5075545323b3d35acff9b95
Chloe Mintel <mintelc12@gmail.com>
2022-06-08 10:56:03 -0400

    pretty much done, just playing with css

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ console.log("connected")
 const searchBox = document.querySelector("#searchBox")
 const search = document.querySelector("#search")
 const searchResults = document.querySelector("#search-results")
+const nowPlaying = document.querySelector("#now-playing")
 
 function buildPage(itunesData) {
     for (let term of itunesData)
@@ -26,18 +27,34 @@ search.addEventListener ("click", (event) => {
     })
     .then(function (data){
         console.log("Response from iTunes API: ", data)
+
+    
     
         searchResults.innerHTML = ""
+
+        if (data.results < 1) {
+            let emptyElement = document.createElement("no-results")
+            emptyElement.innerText = "No results found!"
+            searchResults.appendChild(emptyElement)
+            
+        }
 
     for (let song of data.results) {
         let songDiv = document.createElement("div")
         songDiv.classList.add("song-info")
+    
 
     // Song Name
-        let songElement = document.createElement("song")
-        // songElement.classList.add("songresult")
+        let songElement = document.createElement("div", "song")
+        songElement.classList.add("song")
         songElement.innerText = `"${song.trackName}"`
         songDiv.appendChild(songElement)
+
+    // Hidden Element
+        let hiddenElement = document.createElement("div")
+        hiddenElement.classList.add("hidden")
+        hiddenElement.innerText = song.previewUrl
+        songDiv.appendChild(hiddenElement)
 
     // Artist Name
         let artistElement = document.createElement("artist")
@@ -49,27 +66,43 @@ search.addEventListener ("click", (event) => {
         let releaseDateElement = document.createElement("release")
         let releaseDateFormat = moment(song.releaseDate).format("MMM Do, YYYY")
         // releaseDateElement.classList.add("songresult")
-        releaseDateElement.innerText = releaseDateFormat
+        releaseDateElement.innerText = `Release Date: ${releaseDateFormat}`
         songDiv.appendChild(releaseDateElement)
 
-    // Album Art...let's see if this works
+    // Album Art
         let albumArtElement = document.createElement("img")
         // albumArtElement.classList.add("songresult")
         albumArtElement.src = song.artworkUrl100
         songDiv.appendChild(albumArtElement)
         
-       
 
     searchResults.appendChild(songDiv)    
     
     }
-    
+
+})     .catch (err => {
+    window.alert("Error detected")
 })
 })
 
-  // function EnableDisable(txtPassportNumber) {
-    //     if (txtPassportNumber.value.trim() != "") {
-    //     search.disabled = false;
-    // } else {
-    //     search.disabled = true;
-    // }}
+let songSource = document.createElement("source")
+searchResults.addEventListener("click", function(event) {
+    let target = event.target
+    console.log(target)
+    if (event.target.classList.contains("song")) {
+        console.log(`target: ${target}`)
+        nowPlaying.src = target.nextElementSibling.innerText
+        console.log(nowPlaying.src)
+    }
+})
+
+
+// function nowPlaying(data) {
+//     console.log(data)
+// }
+//   function EnableDisable(txtPassportNumber) {
+//         if (txtPassportNumber.value.trim() != "") {
+//         search.disabled = false;
+//     } else {
+//         search.disabled = true;
+//     }}
